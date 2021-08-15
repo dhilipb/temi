b5541a080e004d1a37784947481907da5c4b051d
Dhilip Buddha <dhilip.buddha@blackrock.com>
2021-08-15 16:33:26 +0530

    Changing to buttons

--- app/src/main/java/com/infitech/temi/MainActivity.java
+++ app/src/main/java/com/infitech/temi/MainActivity.java
@@ -4,121 +4,183 @@ import com.robotemi.sdk.Robot;
 import com.robotemi.sdk.TtsRequest;
 import com.robotemi.sdk.listeners.OnRobotReadyListener;
 
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.infitech.temi.R;
+import org.jetbrains.annotations.NotNull;
 
-public class MainActivity extends AppCompatActivity
-		implements Robot.ConversationViewAttachesListener, Robot.AsrListener, OnRobotReadyListener {
+public class MainActivity extends AppCompatActivity implements Robot.AsrListener, Robot.WakeupWordListener, OnRobotReadyListener {
 
-	private Robot robot;
-	private String lastQuestion;
+    private Robot robot;
+    private String lastQuestion;
+    private TextView txtQuestion;
+    private TextView txtAnswer1;
+    private TextView txtAnswer2;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    /**
+     * Setting up all the event listeners
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        robot.addOnRobotReadyListener(this);
+        robot.addAsrListener(this);
+        robot.addWakeupWordListener(this);
+    }
 
-		try {
-			robot = Robot.getInstance(); // get an instance of the robot in order to begin using its features.
-		} catch (Exception ex) {
-			Log.e("21 Create Exception", ex + "");
-		}
-	}
 
-	@Override
-	public void onRobotReady(boolean b) {
-		Robot.getInstance().hideTopBar();
-	}
+    @Override
+    public void onWakeupWord(@NotNull String wakeupWord, int direction) {
+        onAsrResult("Hello");
+    }
 
-	@Override
-	public void onConversationAttaches(boolean isAttached) {
-		// Do something as soon as the conversation is displayed.
-		Log.e("Temi onConversation", "isAttached:" + isAttached);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		if (!isAttached) {
+        txtQuestion = findViewById(R.id.question);
+        txtAnswer1 = findViewById(R.id.answer1);
+        txtAnswer2 = findViewById(R.id.answer2);
 
-		}
-	}
+        try {
+            robot = Robot.getInstance(); // get an instance of the robot in order to begin using its features.s
+        } catch (Exception ex) {
+            Log.e("21 Create Exception", ex + "");
+        }
+    }
 
-	@Override
-	public void onAsrResult(final @NonNull String asrResult) {
+    /**
+     * Places this application in the top bar for a quick access shortcut.
+     */
+    @Override
+    public void onRobotReady(boolean isReady) {
+        if (isReady) {
+            try {
+                final ActivityInfo activityInfo = getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
+                // Robot.getInstance().onStart() method may change the visibility of top bar.
+                robot.onStart(activityInfo);
+                robot.requestToBeKioskApp();
+                robot.showTopBar();
 
-		if (asrResult.equalsIgnoreCase("Hello")) {
-			speak("Hello, I'm Temi, a robot assistant");
-			ask("Could you please confirm if your name is Mr. Roger Woods?");
+                txtQuestion.setText("Say " + robot.getWakeupWord());
+            } catch (PackageManager.NameNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
-			lastQuestion = "NAME";
-		}
+    public void startQuestion() {
+        setQuestionAnswers("Hello, I'm Temi, a robot assistant. Could you please confirm if your name is Mr. Roger Woods?", "Yes", "No");
 
-		if (lastQuestion.equals("NAME")) {
-			if (asrResult.equalsIgnoreCase("Yes") || asrResult.equalsIgnoreCase("Confirm")) {
-				speak("Thank you for the confirmation");
-				speak("Mr. Roger Woods, it's time for your afternoon medicine");
-				askMedicine();
-			} else if (asrResult.equalsIgnoreCase("No")) {
-				callNurse();
-			}
-		}
+        txtAnswer1.setOnClickListener(view -> {
+            speak("Thank you for the confirmation. Mr. Roger Woods, it's time for your afternoon medicine");
+            askMedicine();
+        });
+        txtAnswer2.setOnClickListener(view -> {
+            callNurse();
+        });
+    }
 
-		if (lastQuestion.equals("MEDICINES")) {
-			if (asrResult.equalsIgnoreCase("Yes") || asrResult.equalsIgnoreCase("Confirm")) {
-				ask("How are you feeling today?");
-				lastQuestion = "FEELING";
-			} else if (asrResult.equalsIgnoreCase("No")) {
-				askMedicine();
-			}
-		}
 
-		if (lastQuestion.equals("FEELING")) {
-			if (asrResult.equalsIgnoreCase("I'm fine") || asrResult.equalsIgnoreCase("Fine")) {
-				speak("Great to hear that");
-				speak("Please use the nurse call button if you need any assitance. Thank you");
-			} else if (asrResult.equalsIgnoreCase("No") || asrResult.equalsIgnoreCase("No")) {
-				callNurse();
-			} else if (asrResult.equalsIgnoreCase("Not great") || asrResult.equalsIgnoreCase("Not great, but okay")
-					|| asrResult.equalsIgnoreCase("Okay")) {
-				ask("Should I call a nurse for assistance?");
-				lastQuestion = "NURSE";
-			}
-		}
+    @Override
+    public void onAsrResult(final @NonNull String asrResult) {
+        if (asrResult.equalsIgnoreCase("Hello")) {
+            setQuestionAnswers("Hello, I'm Temi, a robot assistant. Could you please confirm if your name is Mr. Roger Woods?", "Yes", "No");
+            lastQuestion = "NAME";
+        }
 
-		if (lastQuestion.equals("NURSE")) {
-			if (asrResult.equalsIgnoreCase("Yes") || asrResult.equalsIgnoreCase("Confirm")) {
-				callNurse();
-			} else if (asrResult.equalsIgnoreCase("No")) {
-				speak("Please use the nurse call button if you need any assitance. Thank you");
-			}
-		}
-	}
-	private void askMedicine() {
-		speak("Please take 1 Panadol with a glass of water adn 5ml of Adol Oral Suspension");
-		ask("Have you taken your medicines?");
-		lastQuestion = "MEDICINES";
-	}
+        if (lastQuestion.equals("NAME")) {
+            if (asrResult.equalsIgnoreCase("Yes")) {
+                speak("Thank you for the confirmation. Mr. Roger Woods, it's time for your afternoon medicine");
+                askMedicine();
+            } else if (asrResult.equalsIgnoreCase("No")) {
+                callNurse();
+            }
+        }
 
-	private void callNurse() {
-		speak("I have called the Nurse for Assistance. They will be here shortly");
-		lastQuestion = "";
+        if (lastQuestion.equals("MEDICINES")) {
+            if (asrResult.equalsIgnoreCase("Yes")) {
+                setQuestionAnswers("How are you feeling today?", "I'm fine", "Not great");
+                lastQuestion = "FEELING";
+            } else if (asrResult.equalsIgnoreCase("No")) {
+                askMedicine();
+            }
+        }
 
-		robot.finishConversation();
-	}
+        if (lastQuestion.equals("FEELING")) {
+            if (asrResult.equalsIgnoreCase("I'm fine")) {
+                speak("Great to hear that! Please use the nurse call button if you need any assistance. Thank you");
+            } else if (asrResult.equalsIgnoreCase("Not great")) {
+                setQuestionAnswers("Should I call a nurse for assistance?", "Yes", "No");
+                lastQuestion = "NURSE";
+            }
+        }
+
+        if (lastQuestion.equals("NURSE")) {
+            if (asrResult.equalsIgnoreCase("Yes")) {
+                callNurse();
+            } else if (asrResult.equalsIgnoreCase("No")) {
+                speak("Please use the nurse call button if you need any assistance. Thank you");
+            }
+        }
+    }
+
+    private void askMedicine() {
+        setQuestionAnswers("Please take 1 Panadol with a glass of water adn 5ml of Adol Oral Suspension. Have you taken your medicines?", "Yes", "No");
+        lastQuestion = "MEDICINES";
+    }
+
+    private void callNurse() {
+        setQuestionAnswers("I have called the Nurse for Assistance. They will be here shortly", null, null);
+        lastQuestion = "I have called the Nurse for Assistance. They will be here shortly";
+    }
 
     private void speak(String text) {
-        robot.speak(TtsRequest.create(text, true));
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(text);
+        robot.speak(TtsRequest.create(text, false));
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        txtQuestion.setText(text);
     }
 
-    private void ask(String text) {
-        robot.askQuestion(text);
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(text);
-    }
+    private void setQuestionAnswers(String question, String answer1, String answer2) {
+        speak(question);
 
+        txtQuestion.setText(question);
+
+        if (txtAnswer1 == null) {
+			txtAnswer1.setVisibility(View.INVISIBLE);
+			txtAnswer1.setText("");
+			txtAnswer1.setOnClickListener(view -> {});
+		} else {
+			txtAnswer1.setVisibility(View.VISIBLE);
+            txtAnswer1.setText(answer1);
+            txtAnswer1.setOnClickListener(view -> {
+                onAsrResult(answer1);
+            });
+        }
+
+		if (txtAnswer2 == null) {
+			txtAnswer2.setVisibility(View.INVISIBLE);
+			txtAnswer2.setText("");
+			txtAnswer2.setOnClickListener(view -> {});
+		} else {
+			txtAnswer2.setVisibility(View.VISIBLE);
+			txtAnswer2.setText(answer2);
+			txtAnswer2.setOnClickListener(view -> {
+				onAsrResult(answer2);
+			});
+		}
+    }
 
 }
